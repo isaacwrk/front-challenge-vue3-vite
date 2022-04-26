a76a2757da1621d102010a8ae1c788474251a306
isaacwrk <isaacsouzawrk@gmail.com>
2022-04-25 21:03:16 -0300

    style/feature: adding eslint + creating http file

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
-    <p class="bg-red-200 text-red-500 text-center">alou</p>
-  </div>
+    <div>
+        <p class="bg-red-200 text-red-500 text-center">alou</p>
+        <a href="asdsd">asdsadasds</a>
+    </div>
 </template>
